263b1e5a2e2e695519199ff6848840da25dd2257
Danes <danesnovigar@gmail.com>
2024-01-02 20:58:45 +0700

    Update end2end test, works properly until step "The Product Success Added to Shopping Cart" 10

--- steps/EndToEndTest.py
+++ steps/EndToEndTest.py
@@ -100,65 +100,78 @@ class EndToEndTest:
     @then(u'Detail Page about the Product appears')
     def step_impl(context):
         wait = WebDriverWait(context.driver, timeout=25, poll_frequency=1, ignored_exceptions=None)
-        elementProductTitle = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@itemprop='name']")))
-        elementProductPrice = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "price")))
 
+        # 1. Get product title and price element
+        elementProductTitle = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@itemprop='name']")))
+        elementProductPrice = wait.until(EC.presence_of_element_located((By.XPATH, "//span[@data-price-type='finalPrice']")))
+        # 2. Makesure the title and the price of the product is consistent
         assert elementProductTitle.text == context.productTitle
         assert elementProductPrice.text == context.productPrice
-
-        # Size Option
-        elementSizeDiv = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@attribute-code='size']")))
-        elementsSizeOption = elementSizeDiv.find_elements(By.XPATH, "//div[@class='swatch-option text']")
-        firstSizeOption = elementsSizeOption[0]
-        firstSizeOption.click()
-        # Add selected option size to Global context
-        context.productSize = firstSizeOption.text
-        time.sleep(10)
-
-        # Color Option
-        elementColorDiv = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@attribute-code='color']")))
-        elementsColorOption = elementColorDiv.find_elements(By.XPATH, "//div[@class='swatch-option color']")
-        firstColorOption = elementsColorOption[0]
-        firstColorOption.click()
-        time.sleep(10)
-
-
-        context.productColor = firstColorOption.get_attribute("option-label")
-
-        print("---------------------------------------------------------------------------")
-        # print(elementSizeDiv.text)
-        print(context.productSize)
-        print(context.productColor)
-
-        # elementQuantity = wait.until(EC.presence_of_element_located((By.ID, "qty")))
-        # action = ActionChains(context.driver)
-        # action.move_to_element(elementQuantity)
-        #
-        # time.sleep(5)
-        # elementQuantity.clear()
-        # time.sleep(5)
-        # elementQuantity.send_keys("1")
-        # time.sleep(5)
-
-        # elementAddToCartBtn = wait.until(EC.presence_of_element_located((By.ID, "product-addtocart-button")))
-        # elementAddToCartBtn.click()
 
 
     @then(u'I Makesure the Product In Stock')
     def step_impl(context):
-        raise NotImplementedError(u'STEP: Then I Makesure the Product In Stock')
+        wait = WebDriverWait(context.driver, timeout=25, poll_frequency=1, ignored_exceptions=None)
+
+        # Get Stock element on the web page, then makesure is In stock
+        elementStock = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@title='Availability']")))
+        assert elementStock.text == "IN STOCK"
+
 
     @when(u'I Choose Size, Color, and Quantity the Product')
     def step_impl(context):
-        raise NotImplementedError(u'STEP: When I Choose Size, Color, and Quantity the Product')
+        wait = WebDriverWait(context.driver, timeout=25, poll_frequency=1, ignored_exceptions=None)
+
+        # Choose Size
+        # 1. Size Option, Get the grid size option then get all element size option
+        elementSizeDiv = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@attribute-code='size']")))
+        elementsSizeOption = elementSizeDiv.find_elements(By.XPATH, "//div[@class='swatch-option text']")
+        assert len(elementsSizeOption) != 0
+        # 2. Get the first option size of the product, then click the option
+        firstSizeOption = elementsSizeOption[0]
+        firstSizeOption.click()
+        # 3. Add selected option SIZE to Global context
+        context.productSize = firstSizeOption.text
+        # time.sleep(5)
+
+        # Choose Color
+        # 1. Color Option, Get the grid color option then get all element color option
+        elementColorDiv = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@attribute-code='color']")))
+        elementsColorOption = elementColorDiv.find_elements(By.XPATH, "//div[@class='swatch-option color']")
+        assert len(elementsColorOption) != 0
+        # 2. Get the first option color of the product. then click the option
+        firstColorOption = elementsColorOption[0]
+        firstColorOption.click()
+        # 3. Add selected option COLOR to Global context
+        context.productColor = firstColorOption.get_attribute("option-label")
+        # time.sleep(5)
+
+        # Quantity
+        # 1. Get Quantity element and move to the element until visible on screen
+        elementQuantity = wait.until(EC.presence_of_element_located((By.ID, "qty")))
+        action = ActionChains(context.driver)
+        action.move_to_element(elementQuantity)
+        action.perform()
+        # 2. Clear Quantity input then input 1
+        elementQuantity.clear()
+        elementQuantity.send_keys("1")
+
 
     @when(u'I Click Add to Cart button')
     def step_impl(context):
-        raise NotImplementedError(u'STEP: When I Click Add to Cart button')
+        wait = WebDriverWait(context.driver, timeout=25, poll_frequency=1, ignored_exceptions=None)
+
+        # Search element and Click Add to Cart button
+        elementAddToCartBtn = wait.until(EC.presence_of_element_located((By.ID, "product-addtocart-button")))
+        elementAddToCartBtn.click()
 
     @then(u'The Product Success Added to Shopping Cart')
     def step_impl(context):
-        raise NotImplementedError(u'STEP: Then The Product Success Added to Shopping Cart')
+        wait = WebDriverWait(context.driver, timeout=25, poll_frequency=1, ignored_exceptions=None)
+
+        # Search success alert element and Check the success message
+        elementAlert = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@data-ui-id='message-success']")))
+        assert elementAlert.text == "You added " + context.productTitle + " to your shopping cart."
 
     @when(u'I Click Shopping Cart from Success Message')
     def step_impl(context):
